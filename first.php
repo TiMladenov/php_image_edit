07987249c3a2e8228fa6b5303f093cf505a0806a
timladenov <tihomir.mladenov777@gmail.com>
2019-03-14 23:31:43 +0100

    Added word wrap to image size, writing to image added

--- first.php
+++ first.php
@@ -1,26 +1,47 @@
 <?php
-//TODO: Add checks that image and names are entered
 
-//TODO: Check image size in b, too big is to be discarted
-//TODO: Add image resizing to a square
-//TODO: Add users' names on top of the image
-
-function imgResize ($type, $src, $dest, $width, $height, $quality) {
+function imgResize ($type, $src, $dest, $width, $height, $quality, $userText = null) {
     $tmp_image = "";
+
     if($type == "png") {
+
         $tmp_image = imagecreatefrompng($src);
         $new_tmp_image = imagecreatetruecolor($width, $height);
-
         $color = imagecolorallocate($new_tmp_image, 0, 0, 0);
-        $text = "Test text";
 
+        $tmp_text = "";
+        $box_img = null;
+
+        $font = getcwd() . "/fonts/open-sans/OpenSans-Bold.ttf";
+        $font_size = 30;
+
+        list($image_width, $image_height) = getimagesize($src);
+        $image_margin = 5;
+
+        if(null == $userText) {
+            $userText = "Test Text";
+        } else {
+            $userText = explode(" ", $userText);
+            foreach($userText as $word) {
+                $box_img = imagettfbbox($font_size, 0, $font, $tmp_text . " " . $word);
+                
+                if($box_img[2] > $image_width - $image_margin * 2) {
+                    $tmp_text .= "\n".$word;
+                } else {
+                    $tmp_text .= " ".$word;
+                }
+            }
+        }
+
+        $tmp_text = trim($tmp_text);
         
         imagecopyresampled($new_tmp_image, $tmp_image, 0, 0, 0, 0, $width, $height, $width, $height);
-        //imagestring($new_tmp_image, 5, imagesx($new_tmp_image)/2, 15, $text, $color);
-        imagettftext($new_tmp_image, 30, 0, 150, 50, $color, "/home/tmladenov/Desktop/php_simple_project/fonts/open-sans/OpenSans-Light.ttf", $text);
+        imagettftext($new_tmp_image, $font_size, 0, imagesx($src) / 2, 50, $color, $font, $tmp_text);
         imagepng($new_tmp_image, $dest, $quality);
+
         imagedestroy($tmp_image);
         imagedestroy($new_tmp_image);
+        imagedestroy($box_img);
     } elseif($type == "jpg" || $type == "jpeg") {
         $tmp_image = imagecreatefromjpeg($src);
     }
@@ -33,7 +54,8 @@ $new_filename_size = "";
 
 $image_file_type = "";
 
-if(isset($_POST['upload']) && isset($_POST['firstname']) && isset($_POST['lastname'])) {
+if(isset($_POST['upload']) && ((isset($_POST['firstname']) && $_POST['firstname'] != "") 
+                            && (isset($_POST['lastname']) && $_POST['lastname'] != ""))) {
     $new_file = basename($_FILES['uFile']['name']);
     $new_filename = $uploadDir . $new_file;
     $new_filename_size = filesize($uploadDir . $new_file);
@@ -62,10 +84,14 @@ if(isset($_POST['upload']) && isset($_POST['firstname']) && isset($_POST['lastna
         if($new_filename_size > 0) {
             echo "The file has been successfully saved on this machine!";
         } else {
-            die("There was an issue with saving the file. It might be too big");
+            die("There was an issue with saving the file. It might be too big.");
         }
     }
-    imgResize($image_file_type, $new_filename, $uploadDir . time() . "." . $image_file_type, 600, 600, 9);
+
+    $userFormText = $_POST['firstname'] . " " . $_POST['lastname'];
+
+    imgResize($image_file_type, $new_filename, $uploadDir . time() . "." . $image_file_type, 600, 600, 9, $userFormText);
+
 } else {
     die("Please fill in the all the information in the fields.");
 }
