39d0fd672a749868846354c3f6b366ed0a74f2f3
timladenov <tihomir.mladenov777@gmail.com>
2019-03-17 03:33:00 +0100

    Updated functionality, logic. Add second text to lower part of the picture (optional). Logic needs testing. 1 MB max file upload

--- first.php
+++ first.php
@@ -3,78 +3,95 @@
 function imgResize ($type, $src, $dest, $width, $height, $quality, $userText = null) {
     $tmp_image = "";
 
-    if($type == "png") {
-
+    if("png" == $type) {
         $tmp_image = imagecreatefrompng($src);
-        $new_tmp_image = imagecreatetruecolor($width, $height);
-        $color = imagecolorallocate($new_tmp_image, 0, 0, 0);
-
-        $tmp_text = "";
-        $box_img = null;
-
-        $font = getcwd() . "/fonts/open-sans/OpenSans-Bold.ttf";
-        $font_size = 30;
-
-        list($image_width, $image_height) = getimagesize($src);
-        $image_margin = 5;
-
-        if(null == $userText) {
-            $userText = "Test Text";
-        } else {
-            $userText = explode(" ", $userText);
-            foreach($userText as $word) {
-                $box_img = imagettfbbox($font_size, 0, $font, $tmp_text . " " . $word);
-                
-                if($box_img[2] > $image_width - $image_margin * 2) {
-                    $tmp_text .= "\n".$word;
-                } else {
-                    $tmp_text .= " ".$word;
-                }
-            }
-        }
-
-        $tmp_text = trim($tmp_text);
-        
-        imagecopyresampled($new_tmp_image, $tmp_image, 0, 0, 0, 0, $width, $height, $width, $height);
-        imagettftext($new_tmp_image, $font_size, 0, imagesx($src) / 2, 50, $color, $font, $tmp_text);
-        imagepng($new_tmp_image, $dest, $quality);
-
-        imagedestroy($tmp_image);
-        imagedestroy($new_tmp_image);
-        imagedestroy($box_img);
-    } elseif($type == "jpg" || $type == "jpeg") {
+    } elseif("jpg" == $type) {
         $tmp_image = imagecreatefromjpeg($src);
     }
+
+    $font = getcwd() . "/fonts/open-sans/OpenSans-Bold.ttf";
+    if(isset($_POST['font_size'])) {
+        $font_size = $_POST['font_size'];
+    } else {
+        $font_size = 30;
+    }
+
+    $new_tmp_image = imagecreatetruecolor($width, $height);
+    $color = imagecolorallocate($new_tmp_image, 0, 0, 0);
+    $background_color = imagecolorallocate($new_tmp_image, 255, 255, 255);
+
+    $tmp_text = "";
+    $box_img = null;
+
+    list($image_width, $image_height) = getimagesize($src);
+    $image_margin = 5;
+
+    if(null == $userText) {
+        $userText = "Test Text";
+    } else {
+        $userText = explode(" ", $userText);
+        foreach($userText as $word) {
+            $box_img = imagettfbbox($font_size, 0, $font, $tmp_text . " " . $word);
+            
+            if($box_img[2] > $image_width - $image_margin * 2) {
+                $tmp_text .= "\n".$word;
+            } else {
+                $tmp_text .= " ".$word;
+            }
+        }
+    }
+
+    $tmp_text = trim($tmp_text);
+    
+    imagefilledrectangle($tmp_image, 0, 0, $width, $font_size * 2, $background_color);
+    imagecopyresampled($new_tmp_image, $tmp_image, 0, 0, 0, 0, $width, $height, $width, $height);
+    imagettftext($new_tmp_image, $font_size, 0, 0, $font_size * 1.5, $color, $font, $tmp_text);
+    
+    if("jpg" == $type) {
+        imagejpeg($new_tmp_image, $dest, $quality);
+    } else {
+        imagepng($new_tmp_image, $dest, $quality);
+    }
+
+    imagedestroy($tmp_image);
+    imagedestroy($new_tmp_image);
+    imagedestroy($box_img);
 }
+
+/*=================== PROGRAM BEGIN ===================*/
 
 $uploadDir =  getcwd() . "/uploads/";
 $new_file = "";
 $new_filename = "";
 $new_filename_size = "";
+$new_image_height = "";
+$new_image_width = "";
 
 $image_file_type = "";
 
-if(isset($_POST['upload']) && ((isset($_POST['firstname']) && $_POST['firstname'] != "") 
-                            && (isset($_POST['lastname']) && $_POST['lastname'] != ""))) {
+if(isset($_POST['upload']) && ((isset($_POST['top_text']) && $_POST['top_text'] != "") 
+                            && (isset($_POST['bottom_text']) && $_POST['bottom_text'] != ""))) {
+
     $new_file = basename($_FILES['uFile']['name']);
     $new_filename = $uploadDir . $new_file;
     $new_filename_size = filesize($uploadDir . $new_file);
 
     $image_file_type = strtolower(pathinfo($new_filename, PATHINFO_EXTENSION));
 
-    if($image_file_type != "png" && $image_file_type != "jpg" && $image_file_type != "jpeg" ) {
-        die("You cannot upload this type of image. Only supported formats are png, jpg, jpeg.");
+    if($image_file_type != "png" && $image_file_type != "jpg") {
+        die("You cannot upload this type of image. Only supported formats are png or jpg.");
     }
 
     if(file_exists($new_filename)) {
-        $new_file = time() . "_" . basename($_FILES['uFile']['name']);
+
+        $new_file = date("dmy_his") . "_" . basename($_FILES['uFile']['name']);
         $new_filename = $uploadDir . $new_file;
         copy($_FILES['uFile']['tmp_name'], $new_filename);
 
         if($new_filename_size > 0) {
             echo "The file has been renamed and successfully saved on this machine!";
             echo "<br/>";
-            echo "Thie file name is " . $new_file . " and file size is " . $_FILES['uFile']['size'];
+            echo "Thie file name is " . $new_file . " and file size is " . $new_filename_size / 1000 . "bytes.";
         } else {
             die("There seem to have been an issue with saving the file.");
         }
@@ -83,14 +100,25 @@ if(isset($_POST['upload']) && ((isset($_POST['firstname']) && $_POST['firstname'
         $new_filename_size = filesize($uploadDir . $new_file);
         if($new_filename_size > 0) {
             echo "The file has been successfully saved on this machine!";
+            echo "<br/>";
+            echo "Thie file name is " . $new_file . " and file size is " . $new_filename_size / 1000 . "bytes.";
         } else {
-            die("There was an issue with saving the file. It might be too big.");
+            die("There was an issue with saving the file. It might be too big. File size is " . $new_filename_size / 1000 . " bytes.");
         }
     }
 
-    $userFormText = $_POST['firstname'] . " " . $_POST['lastname'];
+    $userFormText = $_POST['top_text'] . " " . $_POST['bottom_text'];
 
-    imgResize($image_file_type, $new_filename, $uploadDir . time() . "." . $image_file_type, 600, 600, 9, $userFormText);
+    if(isset($_POST['orientation_x']) && isset($_POST['orientation_y'])) {
+        $new_image_height = $_POST['orientation_y'];
+        $new_image_width = $_POST['orientation_x'];
+    } else {
+        $new_image_height = 800;
+        $new_image_width = 800;
+    }
+
+    imgResize($image_file_type, $new_filename, $uploadDir . date("dmy_his") . "_new" . "." . $image_file_type, 
+           (int) $new_image_width, (int) $new_image_height, 9, $userFormText);
 
 } else {
     die("Please fill in the all the information in the fields.");
