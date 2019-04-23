d452a895d6f5b5ea46cc3f6e6646945654035cd0
timladenov <tihomir.mladenov777@gmail.com>
2019-04-23 20:54:17 +0300

    Updated install script to give permissions to Apache for the uploads folder

--- setup_uploads_dir.sh
+++ setup_uploads_dir.sh
@@ -6,6 +6,9 @@
 cd && cd Desktop
 if [ ! -d "uploads" ] ; then
 	mkdir uploads
+	sudo chown www-data:www-data "uploads"
+	sudo chmod 755 "uploads"
+	sudo service apache2 restart
 	echo "Created directory Uploads in Desktop."
 else
 	echo "Checking in " $(pwd) "."
